b4bdee2d307bb2664f28b638149f40b738ab12a4
yepengding <dingyepeng@gmail.com>
2022-03-31 21:03:44 +0900

    Update typing.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,30 @@
+export interface Entry {
+    id: string,
+    type: string,
+    title: string,
+    author?: string,
+    booktitle?: string,
+    publisher?: string,
+    year?: string | number,
+    month?: string,
+    pages?: string,
+    journal?: string,
+    num?: string | number,
+    volume?: string,
+    doi?: string,
+    issn?: string,
+    url?: string,
+    urldate?: string,
+    language?: string,
+    copyright?: string,
+    note?: string,
+    keyword?: string,
+    abstract?: string
+}
+
 export declare namespace BibtexParser {
-    function parseToJSON(input: string): any;
+    function parseToJSON(input: string): [Entry];
+
     function parseToJSONString(input: string): string;
+
 }
